a5aff6e97b9bf90da4b7efab3abe0929ba16ac34
Raian Medeiros <raiannolaco@gmail.com>
2023-04-24 00:00:45 -0300

    Iniciando Cap 4

---
+++ LivroOO_dotNet.Capitulo_4/Departamento.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivroOO_dotNet.Capitulo_4
+{
+    public class Departamento
+    {
+        public string Nome { get; set; }
+        public IList<Curso> Cursos { get; } = new List<Curso>();
+
+        public void RegistrarCurso(Curso NovoCurso)
+        {
+            Cursos.Add(NovoCurso);
+        }
+
+        public int ObterQuantidadeCurso()
+        {
+            return Cursos.Count;
+        }
+
+        public Curso ObterCursoPorIndice(int indice)
+        {
+            return Cursos[indice];
+        }
+    }
+}
