0f56a943c492e09cf99a88ce3415a6e77b4f94d0
Raian Medeiros <raiannolaco@gmail.com>
2023-05-16 02:30:11 -0300

    Classe funcionario e gerente

---
+++ LivroOO_dotNet.Capitulo_4/Funcionario/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivroOO_dotNet.Capitulo_4.Funcionario
+{
+    public  class Funcionario
+    {
+        public string Nome { get; set; }
+        private Gerente gerente;
+        public Gerente Gerente {
+            get { return this.gerente}
+            set
+            {
+                if (this.gerente != null)
+                    this.gerente.Funcionarios.Remove(this);
+
+                this.gerente = value;
+
+                if(this.gerente != null)
+            }
+        }
+    }
+}
---
+++ LivroOO_dotNet.Capitulo_4/Funcionario/Gerente.cs
@@ -0,0 +1,8 @@
+﻿namespace LivroOO_dotNet.Capitulo_4.Funcionario
+{
+    public class Gerente
+    {
+        public string Nome { get; set; }
+        public List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+    }
+}
