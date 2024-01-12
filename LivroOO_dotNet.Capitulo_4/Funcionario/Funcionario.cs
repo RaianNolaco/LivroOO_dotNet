0e99e7a0b031980180465f805ff83d3a8708b4ee
Raian Medeiros <raiannolaco@gmail.com>
2024-01-12 12:35:13 -0300

    oo

--- LivroOO_dotNet.Capitulo_4/Funcionario/Funcionario.cs
+++ LivroOO_dotNet.Capitulo_4/Funcionario/Funcionario.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LivroOO_dotNet.Capitulo_4.Funcionario
+﻿namespace LivroOO_dotNet.Capitulo_4.Funcionario
 {
     public  class Funcionario
     {
         public string Nome { get; set; }
+
         private Gerente gerente;
         public Gerente Gerente {
             get { return this.gerente; }
---
+++ LivroOO_dotNet.Capitulo_4/Funcionario/execExemploFucionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LivroOO_dotNet.Capitulo_4.Funcionario
+{
+    public class execExemploFucionario
+    {   
+        public static void exec()
+        {
+            Funcionario funcionario1 = new Funcionario();
+            funcionario1.Nome = "joao";
+
+            Funcionario funcionario2 = new Funcionario();
+            funcionario2.Nome = "Ricardo";
+
+            Funcionario funcionario3 = new Funcionario();
+            funcionario3.Nome = "Sophia";
+
+            Funcionario funcionario4 = new Funcionario();
+            funcionario4.Nome = "Richar";
+
+            Funcionario funcionario5 = new Funcionario();
+            funcionario5.Nome = "jose";
+
+
+            Gerente gerente1 = new Gerente();
+            gerente1.Nome= "nilson";
+        }
+        
+    }
+}
